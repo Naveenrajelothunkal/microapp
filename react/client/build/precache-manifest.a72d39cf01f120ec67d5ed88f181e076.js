bccf6ad2910d4fd29065c49f6d5a3bda1e51bbcf
Naveenrajelothunkal <unni.navi@gmail.com>
2020-04-25 14:56:54 +0530

    react sample with build locally

---
+++ react/client/build/precache-manifest.a72d39cf01f120ec67d5ed88f181e076.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081b05cfaabc753901edd643f48a9483",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "34679f6069eca04dce3c",
+    "url": "/static/css/main.608b9c45.chunk.css"
+  },
+  {
+    "revision": "f559620ab3c8b65ac479",
+    "url": "/static/js/2.cf7e3278.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cf7e3278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34679f6069eca04dce3c",
+    "url": "/static/js/main.2950ce21.chunk.js"
+  },
+  {
+    "revision": "6293c283a37eb7f0f6d0",
+    "url": "/static/js/runtime-main.edeac9db.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+])

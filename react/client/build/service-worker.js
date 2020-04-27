171995243345ac498222e353f4d473d6ce322902
Naveenrajelothunkal <unni.navi@gmail.com>
2020-04-27 14:44:54 +0530

    public url

--- react/client/build/service-worker.js
+++ react/client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react/precache-manifest.7154c0f08747d89f1c5d63bd18c3c11c.js"
+  "http://localhost:5000/react/precache-manifest.538b6eeac235de1f316e29deb5c2c3b0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("http://localhost:5000/react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
